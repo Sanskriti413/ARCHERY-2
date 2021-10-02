8298ef9fcc9503bb7b00b28d7527b6ada91efbfd
Sanskriti413 <88034126+Sanskriti413@users.noreply.github.com>
2021-10-02 13:17:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,121 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var palyer, playerBase;
+var computer, computerBase;
+
+var arrow;
+
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
+
+  computerBase = new ComputerBase(
+    width - 300,
+    random(450, height - 300),
+    180,
+    150
+  );
+  computer = new Computer(
+    width - 280,
+    computerBase.body.position.y - 153,
+    50,
+    180
+  );
+  computerArcher = new ComputerArcher(
+    width - 340,
+    computerBase.body.position.y - 180,
+    120,
+    120
+  );
+  
+  arrow = new PlayerArrow(playerArcher.body.position.x, playerArcher.body.position.y, 100, 10);
+  
+}
+
+function draw() {
+  background(180);
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+ 
+  playerBase.display();
+  player.display();
+  
+
+  computerBase.display();
+  computer.display();
+  
+  playerArcher.display();
+  computerArcher.display();
+  
+   if(keyCode === 32){
+     arrow.display()
+     arrow.shoot(playerArcher);
+    }
+
+    if(keyCode === 32){
+     arrow.shoot(playerArcher.body.angle);
+    }
+   
+    if(keyCode === 32){
+     arrow.display()
+     arrow.shoot(playerArcher.body.angle);
+    }
+
+    if(move === "UP"&& computerArcher.body.angle < 1.77){
+      angleValue = 0.1;
+    }else{
+            angleValue = -0.1;
+    }
+     
+     if(move === "Down" && computerArcher.body.angle > 1.47) {
+       angleValue = -0.1;
+     }else{
+       angleValue = 0.1;
+     }
+
+
+
+}
+ 
+shoot(archerAngle) {
+  var velocity = p5.Vector.fromAngle(archerAngle);
+    velocity.mult(20);
+  Matter.Body.setStatic(this.body,false);
+  Matter.Body.setVelocity(this.body, {x: velocity.x, y: velocity.y});
+}
+
+
+
+
+
+
+
+
+
+
+    
+
